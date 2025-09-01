bd5b396cfc90e521963f4aad4d4e39c7d29ee2f9
Mikhael D'Amato <damato578@gmail.com>
2025-09-01 19:00:05 -0300

    chore: add styles to global

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,20 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/assets/styles/variables" as *;
+          @use "@/assets/styles/mixins" as *;
+        `,
+      },
     },
   },
 })
