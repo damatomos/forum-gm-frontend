e45bf61d678c41a561ba5e4edbbdc5b2ced53d05
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-02 22:53:56 -0300

    remove elements of teste in app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import InputComponent from './components/InputComponent.vue';
 </script>
 
 <template>
-  <InputComponent variant="three" placeholder="teste" maxLength="30" label="Bio"/>
   <RouterView />
 </template>
 
