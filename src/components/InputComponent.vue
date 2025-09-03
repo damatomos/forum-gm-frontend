67c72d83e7ab688fd9e25e5b6f9d4135565a1385
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-02 22:50:26 -0300

    style: create style for variants and label

--- src/components/InputComponent.vue
+++ src/components/InputComponent.vue
@@ -17,7 +17,7 @@ const props = defineProps<Props>()
   <div class="container-input">
     <input
       v-model="textInput"
-      :class="[props.variant ? props.variant : 'one']"
+      :class="[variant ? variant : 'one']"
       type="text"
       :placeholder="placeholder"
       :maxlength="maxLength"
@@ -28,4 +28,56 @@ const props = defineProps<Props>()
   </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+label {
+  color: gm-c-text-light-1;
+  font-size: $font-family;
+  font-size: $font-size-xl;
+  margin-bottom: 0.625rem;
+}
+
+.container-input {
+  max-width: fit-content;
+  position: relative;
+
+  input {
+    width: 100%;
+    font-size: $font-size-xl;
+    color: $gm-c-divider-light-2;
+    background-color: $gm-c-white-soft;
+    border-style: none;
+    outline: none;
+    border-radius: 6px;
+    position: relative;
+  }
+
+  .one {
+    padding: 0.625rem 1rem;
+  }
+
+  .two {
+    padding: 0.625rem 3rem 0.625rem 1rem;
+  }
+
+  .three {
+    border: 1px solid $gm-c-divider-light-2;
+    padding: 0.625rem 4rem 0.625rem 1rem;
+  }
+
+  img {
+    height: 24px;
+    width: 24px;
+    position: absolute;
+    top: 0.625rem;
+    right: 1rem;
+    cursor: pointer;
+  }
+
+  span {
+    color: $gm-c-divider-light-2;
+    position: absolute;
+    bottom: 0;
+    right: 0.625rem;
+  }
+}
+</style>
