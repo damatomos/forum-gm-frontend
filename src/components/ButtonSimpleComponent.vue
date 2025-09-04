7263f7686451ea07398f977fe14dd8c6c11fae31
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-04 20:37:37 -0300

    feat: create logic and structure ButtonSimpleComponent

--- src/components/ButtonSimpleComponent.vue
+++ src/components/ButtonSimpleComponent.vue
@@ -1,8 +1,16 @@
 <script setup lang="ts">
+interface Props {
+  content: string
+  variant?: 'purple' | 'rose' | 'button-auth'
+}
+
+const props = defineProps<Props>()
 </script>
 
 <template>
-
+  <button :class="[props.variant ? variant : 'common']">
+    {{ content }}
+  </button>
 </template>
 
 <style scoped lang="scss">
