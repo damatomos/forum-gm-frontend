292b441005be0f9ac39040aad8ee2aabe8d31b34
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-04 20:38:53 -0300

    style: create style for ButtonSimpleComponent

--- src/components/ButtonSimpleComponent.vue
+++ src/components/ButtonSimpleComponent.vue
@@ -14,4 +14,42 @@ const props = defineProps<Props>()
 </template>
 
 <style scoped lang="scss">
+$purple-button: $gm-c-purple-polygon;
+$red-button: $gm-c-red;
+
+@mixin button-purple {
+  background-color: $purple-button;
+  &:hover {
+    background-color: $gm-c-blue-polygon;
+  }
+}
+
+button {
+  width: 100%;
+  font-size: $font-size-base;
+  font-family: $gm-c-principal-font;
+  font-weight: bold;
+  color: $gm-c-white;
+  border-radius: 6px;
+  border: none;
+  padding: 0.625rem 1rem;
+  cursor: pointer;
+}
+
+.rose {
+  background-color: $red-button;
+  &:hover {
+    background-color: $gm-c-red-dark;
+  }
+}
+
+.purple {
+  @include button-purple;
+}
+
+.button-auth {
+  width: 450px;
+  @include button-purple;
+  font-size: $font-size-3xl;
+}
 </style>
