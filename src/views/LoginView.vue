ba53aeccdc5f993a858d4d7130a52cb976206629
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-04 22:39:01 -0300

    feat: create structure loginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,5 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import InputComponent from '@/components/InputComponent.vue'
+import ButtonSimpleComponent from '@/components/ButtonSimpleComponent.vue'
+</script>
 
-<template>LoginView</template>
+<template>
+  <div class="container-login">
+    <h1>Login</h1>
+    <InputComponent
+      label="Email"
+      type="email"
+      variant="auth"
+      placeholder="Seu melhor email"
+      id="email-login"
+    />
+    <InputComponent
+      class="input-senha"
+      label="Password"
+      type="password"
+      variant="auth"
+      id="password-login"
+    />
+    <ButtonSimpleComponent class="btn-login" variant="button-auth" content="Entrar" />
+    <p>Não possui uma conta? <a href="RegisterView.vue">registre-se.</a></p>
+  </div>
+</template>
 
 <style scoped lang="scss"></style>
