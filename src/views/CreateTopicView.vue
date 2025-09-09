de5a299658cbc5f5ba0f38c6eaa52a74808475f8
Mikhael D'Amato <damato578@gmail.com>
2025-09-09 16:59:03 -0300

    feat: add background and navigation to create-topic

--- src/views/CreateTopicView.vue
+++ src/views/CreateTopicView.vue
@@ -1,13 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HeaderComponent from '@/components/HeaderComponent.vue'
+</script>
 
 <template>
+  <HeaderComponent />
   <main>
-    <h1 ref="title">Hello World</h1>
+    <div class="background"></div>
   </main>
 </template>
 
 <style scoped lang="scss">
-h1 {
-  color: #42b983;
+main {
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
+  min-height: calc(100dvh - 50px);
+  .background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+
+    clip-path: polygon(0% 0%, 20% 0%, 55% 100%, 0% 100%);
+
+    background-color: $color-pink;
+  }
 }
 </style>
