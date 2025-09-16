a4b97da72f6f2d88d3a672bbf4e32fa756efa090
joseildoandrade12 <joseildoandrade1405@gmail.com>
2025-09-16 11:30:36 -0300

    feat: addition background in components register and login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,49 +1,58 @@
 <script setup lang="ts">
 import InputComponent from '@/components/InputComponent.vue'
 import ButtonSimpleComponent from '@/components/ButtonSimpleComponent.vue'
+import BackgroundComponent from '@/components/BackgroundComponent.vue'
 </script>
 
 <template>
-  <div class="container-login">
-    <h1>Login</h1>
-    <InputComponent
-      label="Email"
-      type="email"
-      variant="auth"
-      placeholder="Seu melhor email"
-      id="email-login"
-    />
-    <InputComponent label="Password" type="password" variant="auth" id="password-login" />
-    <ButtonSimpleComponent class="btn-login" variant="button-auth" content="Entrar" />
-    <p>Não possui uma conta? <a href="RegisterView.vue">registre-se.</a></p>
+  <BackgroundComponent />
+  <div class="container">
+    <div class="container-login">
+      <h1>Login</h1>
+      <InputComponent
+        label="Email"
+        type="email"
+        variant="auth"
+        placeholder="Seu melhor email"
+        id="email-login"
+      />
+      <InputComponent label="Password" type="password" variant="auth" id="password-login" />
+      <ButtonSimpleComponent class="btn-login" variant="button-auth" content="Entrar" />
+      <p>Não possui uma conta? <a href="RegisterView.vue">registre-se.</a></p>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container-login {
-  @include style-login-and-register;
+.container {
+  width: 100%;
+  height: 100vh;
+  @include flex-center;
+  .container-login {
+    @include style-login-and-register;
 
-  h1 {
-    @include text-center-white;
-    font-size: $font-size-5xl;
-    font-weight: bold;
-    margin-bottom: 1.5rem;
-  }
+    h1 {
+      @include text-center-white;
+      font-size: $font-size-5xl;
+      font-weight: bold;
+      margin-bottom: 1.5rem;
+    }
 
-  :deep(#email-login) {
-    margin-bottom: 1.5rem;
-  }
+    :deep(#email-login) {
+      margin-bottom: 1.5rem;
+    }
 
-  :deep(#password-login) {
-    margin-bottom: 3rem;
-  }
+    :deep(#password-login) {
+      margin-bottom: 3rem;
+    }
 
-  .btn-login {
-    margin-bottom: 1rem;
-  }
+    .btn-login {
+      margin-bottom: 1rem;
+    }
 
-  p {
-    @include link-login-and-register;
+    p {
+      @include link-login-and-register;
+    }
   }
 }
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,59 +4,67 @@ import ButtonSimpleComponent from '@/components/ButtonSimpleComponent.vue'
 </script>
 
 <template>
-  <div class="container-register">
-    <h1>Registre-se</h1>
-    <InputComponent
-      label="Nome"
-      type="text"
-      variant="auth"
-      placeholder="Seu nome"
-      id="nome-register"
-    />
-    <InputComponent
-      label="Email"
-      type="email"
-      variant="auth"
-      placeholder="Seu melhor email"
-      id="email-register"
-    />
-    <InputComponent
-      label="Password"
-      type="password"
-      variant="auth"
-      placeholder=""
-      id="password-register"
-    />
-    <ButtonSimpleComponent class="btn-login" variant="button-auth" content="Cadastrar" />
-    <p>Já possui uma conta? <a href="LoginView.vue">clique aqui.</a></p>
+  <div class="container">
+    <div class="container-register">
+      <h1>Registre-se</h1>
+      <InputComponent
+        label="Nome"
+        type="text"
+        variant="auth"
+        placeholder="Seu nome"
+        id="nome-register"
+      />
+      <InputComponent
+        label="Email"
+        type="email"
+        variant="auth"
+        placeholder="Seu melhor email"
+        id="email-register"
+      />
+      <InputComponent
+        label="Password"
+        type="password"
+        variant="auth"
+        placeholder=""
+        id="password-register"
+      />
+      <ButtonSimpleComponent class="btn-login" variant="button-auth" content="Cadastrar" />
+      <p>Já possui uma conta? <a href="LoginView.vue">clique aqui.</a></p>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.container-register {
-  @include style-login-and-register;
-  h1 {
-    @include text-center-white;
-    font-size: $font-size-5xl;
-    font-weight: bold;
-    margin-bottom: 1.5rem;
-  }
+.container {
+  width: 100%;
+  height: 100vh;
+  @include flex-center;
 
-  :deep(#nome-register),
-  :deep(#email-register) {
-    margin-bottom: 1.5rem;
-  }
+  .container-register {
+    @include style-login-and-register;
+    h1 {
+      @include text-center-white;
+      font-size: $font-size-5xl;
+      font-weight: bold;
+      margin-bottom: 1.5rem;
+    }
 
-  :deep(#password-register) {
-    margin-bottom: 3rem;
-  }
+    :deep(#nome-register),
+    :deep(#email-register) {
+      margin-bottom: 1.5rem;
+    }
 
-  .btn-login {
-    margin-bottom: 1rem;
-  }
+    :deep(#password-register) {
+      margin-bottom: 3rem;
+    }
 
-  p {
-    @include link-login-and-register;
+    .btn-login {
+      margin-bottom: 1rem;
+    }
+
+    p {
+      @include link-login-and-register;
+    }
   }
 }
 </style>
